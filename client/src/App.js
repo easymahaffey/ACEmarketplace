b86e6dd9b19c8194eb95a551908e7bf19491aff2
drelarson <larsonandre01@gmail.com>
2023-09-14 09:13:07 -0700

    created browser router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ACE Marketplace App!</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>ACE Marketplace App!</h1>
+      </div>
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route>
+
+          </Route>
+
+        </Routes>
+
+
+      </BrowserRouter>
+
+
+
+
+
+    </>
+
   );
 }
 
