058ef3515bdc6670e74d787603ccfd52a1e3a688
drelarson <larsonandre01@gmail.com>
2023-09-15 11:24:03 -0700

    created search component and placement

---
+++ client/src/components/search/SearchBar.jsx
@@ -0,0 +1,13 @@
+import './search.scss'
+
+const SearchBar = ()=>{
+    return(
+        <div id="search-area">
+            <div className="search-bar"></div>
+            <button className='buttons'>Search</button>
+        </div>
+
+    )
+}
+
+export default SearchBar
